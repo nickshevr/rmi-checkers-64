34c36d08e384824db791996402f4d2682d0a8180
nickshevr <nickshevr@google.com>
2017-01-22 19:41:13 +0300

    Added icons and fixes

--- src/checkers/Board.java
+++ src/checkers/Board.java
@@ -1,5 +1,131 @@
 package checkers;
 
-public class Board {
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.Serializable;
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
 
+public class Board implements Remote, Serializable {
+	private static final long serialVersionUID = 1L;
+
+	private static final int NUM_PIECES = 36; // the maximum number of pieces in any game
+	public final int gridSize;
+	private char blackCorner;
+	
+	private Piece[] piecePlacement;
+
+	/**
+	 * NOTES
+	 * 
+	 * The top of the board is y=0, so to move pieces down, ADD to y
+	 */
+
+
+	// regular constructor
+	public Board(int gridSize) throws Exception {
+		this.gridSize = gridSize;
+		int currentPiece;
+		int i, j;
+
+		// initialize the pieces
+		piecePlacement = new Piece[NUM_PIECES];
+		for(i=0; i<piecePlacement.length; i++)
+			piecePlacement[i] = null;
+
+		currentPiece = 0;
+
+		// create all of the black pieces
+		for (i = 0; i < 3; i++) {
+			for (j = 0; j < this.gridSize; j++) {
+				if ((i + j) % 2 == 1) {
+					// NOTE: i == y  and  j == x
+					Piece piece = new Piece(new Position(j, i), false, Piece.WHITE);
+					piecePlacement[currentPiece++] = piece;
+				}
+			}
+		}
+
+		// create all of the white pieces
+		for (i = gridSize - 1; i > this.gridSize - 4; i--) {
+			for (j = 0; j <  gridSize; j++) {
+				if ((i + j) % 2 == 1) {
+					// NOTE: i == y  and  j == x
+					Piece piece = new Piece(new Position(j, i), false, Piece.BLACK);
+					piecePlacement[currentPiece++] = piece;
+				}
+			}
+		}
+	}
+
+	// Copy constructor
+	public Board(Board copy) throws RemoteException {
+		this.gridSize = copy.gridSize;
+		Piece[] copyPieces = copy.getPiecePlacement();
+
+		piecePlacement = new Piece[NUM_PIECES];
+		for (int i = 0; i < piecePlacement.length && copyPieces[i] != null; i++) {
+			piecePlacement[i] = new Piece(copyPieces[i]);
+		}
+
+	}
+	
+	// getter for piece placement
+		public Piece[] getPiecePlacement() throws RemoteException {
+			return piecePlacement;
+		}
+
+		// setter for piecePlacement
+		public void setPiecePlacement(Piece[] piecePlacement) throws Exception {
+			if (piecePlacement == null) {
+				throw new Exception("Wrong value");
+			}
+			this.piecePlacement = piecePlacement;
+		}
+
+		@Override
+		public boolean equals(Object obj) {
+			if (!(obj instanceof Board)) {
+				return false;
+			}
+			Board newObj = (Board) obj;
+			try {
+				return Arrays.equals(newObj.getPiecePlacement(),
+								this.getPiecePlacement());
+			} catch (RemoteException e) {
+				return false;
+			}
+		}
+		
+		public Piece getPieceAtPosition(int x, int y){
+			Position pos;
+
+			for(int i=0; i<piecePlacement.length; i++){
+				if (piecePlacement[i] != null) {
+					pos = piecePlacement[i].getPiecePosition();
+					if(pos.getX() == x && pos.getY() == y)
+						return piecePlacement[i];
+				}
+			}
+
+			return null;
+		}
+		
+		public int getPieceIndex(Position p, Piece[] pArray){
+			Position pos;
+
+			for(int i=0; i<pArray.length; i++){
+				if (pArray[i] != null) {
+					pos = pArray[i].getPiecePosition();
+					if(pos.getX() == p.getX() && pos.getY() == p.getY())
+						return i;
+				}
+			}
+
+			return -1;
+		}
 }
+	
--- src/checkers/Piece.java
+++ src/checkers/Piece.java
@@ -1,5 +1,104 @@
 package checkers;
 
-public class Piece {
+import java.io.Serializable;
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+import java.util.ArrayList;
+
+
+public class Piece implements Remote, Serializable{
+	public static final char BLACK = 'b';
+	public static final char WHITE = 'w';
+
+	private Position piecePosition;
+	private Boolean crown;
+	private char colour;
+	
+	private String imageURL;
+
+	public Piece(Piece copy) throws RemoteException {
+		piecePosition = new Position(copy.getPiecePosition());
+		crown = new Boolean(copy.isCrown());
+		colour = copy.getColour();
+		imageURL = new String(copy.getImageURL());
+	}
+
+	public Piece(Position piecePosition, Boolean crown, char colour) {
+		this.piecePosition = piecePosition;
+		this.crown = crown;
+		this.colour = colour;
+
+		if (colour == Piece.WHITE){
+			if(crown)
+				this.imageURL = "/peice8x8Kw.png";
+			else
+				this.imageURL = "/peice8x8w.png";
+		}
+		else{
+			if(crown)
+				this.imageURL = "/peice8x8K.png";
+			else
+				this.imageURL = "/peice8x8.png";
+		}
+	}
+
+	public Position getPiecePosition() {
+		try {
+			return new Position(piecePosition);
+		} catch (Exception e) {};
+		// TODO Implement the catch block?
+
+		return null;
+	}
+
+	public void setPiecePosition(Position piecePosition) {
+		this.piecePosition = piecePosition;
+	}
+
+	public Boolean isCrown() {
+		return crown;
+	}
+
+	public void setCrown(Boolean crown) {
+		this.crown = crown;
+	}
+
+	public char getColour() {
+		return colour;
+	}
+
+	public void setColour(char colour) {
+		this.colour = colour;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (obj instanceof Piece) {
+			Piece pieceToBeCompared = (Piece) obj;
+			Position pieceToBeComparedPosition = pieceToBeCompared
+					.getPiecePosition();
+			char pieceToBeComparedColour = pieceToBeCompared.getColour();
+			return pieceToBeComparedPosition.equals(this.getPiecePosition())
+					&& pieceToBeComparedColour == this.getColour();
+		}
+		return false;
+	}
+
+	public String getImageURL() {
+		return imageURL;
+	}
+
+	public void setPieceImage(String imageLocation) {
+		this.imageURL = imageLocation;
+	}
+	
+	public void turnKing(){
+		if(this.colour == Piece.WHITE)
+			this.imageURL = "/peice8x8Kw.png";
+		else
+			this.imageURL = "/peice8x8K.png";
+		
+		this.crown = true;
+	}
 
 }
